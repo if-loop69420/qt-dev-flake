cbd01800503e88451e114aa1d710f7e467583428
void toni <81078181+if-loop69420@users.noreply.github.com>
2023-09-04 11:46:43 +0200

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,15 @@
 { pkgs ? import <nixpkgs> { }}:
 with pkgs;
 mkShell {
-  buildInputs = [ ];
+  buildInputs = [ 
+    qt6.full
+    qtcreator-qt6
+    cmake
+    vscode
+    vscodium
+  ];
   nativeBuildInputs = [ ];
   shellHook = '' 
-  
+    echo "Welcome to the qt-dev-flake"
   '';
 }
